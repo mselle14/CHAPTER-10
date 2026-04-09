e681a301804f65eaf51d7176271ba34dfe3e4056
mselle14 <mselle14@aum.edu>
2026-04-09 15:01:47 -0500

    Update student quiz.cpp

--- student quiz.cpp
+++ student quiz.cpp
@@ -1,20 +1,43 @@
-// student quiz.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Marshon Sellers
 
 #include <iostream>
+using namespace std;
+
+class QuizResult
+{
+private:
+    int score;
+
+public:
+    void set_score(int new_score)
+    {
+        if (new_score >= 0 && new_score <= 100)
+        {
+            score = new_score;
+        }
+        else
+        {
+            cout << "Invalid score!" << endl;
+        }
+    }
+
+    int get_score()
+    {
+        return score;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    QuizResult quiz;
+    quiz.set_score(0);
+
+    int input_score;
+    cin >> input_score;
+
+    quiz.set_score(input_score);
+
+    cout << "Final score: " << quiz.get_score() << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
