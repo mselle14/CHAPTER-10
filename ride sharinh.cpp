4e0fed742b5efa85eb536550752328080b639154
mselle14 <mselle14@aum.edu>
2026-04-09 15:00:47 -0500

    Update ride sharinh.cpp

--- ride sharinh.cpp
+++ ride sharinh.cpp
@@ -1,20 +1,63 @@
-// ride sharinh.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Marshon Sellers
 
 #include <iostream>
+#include <iomanip>
 
-int main()
+using namespace std;
+class Trip 
 {
-    std::cout << "Hello World!\n";
+private:
+    float distance_km;
+    float rate_per_km;
+
+public:
+    Trip() 
+{
+        distance_km = 0;
+        rate_per_km = 12.5f;
+    }
+
+    void set_distance_km(float d) 
+{
+        if (d >= 0) 
+        {
+            distance_km = d;
+        } else 
+        {
+            cout << "Invalid distance!" << endl;
+        }
+    }
+
+    void set_rate_per_km(float r) 
+        {
+            if (r > 0) 
+        {
+            rate_per_km = r;
+        } 
+        else 
+        {
+            cout << "Invalid rate!" << endl;
+        }
+    }
+
+    float get_fare() 
+{
+        return distance_km * rate_per_km;
+    }
+};
+
+int main() 
+{
+    Trip myTrip;
+    float d, r;
+
+    cin >> d >> r;
+
+    myTrip.set_distance_km(d);
+    myTrip.set_rate_per_km(r);
+
+    cout << fixed << setprecision(2);
+    cout << "Fare: " << myTrip.get_fare() << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
