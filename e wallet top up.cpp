2f6f4207204e622f5718066c32d4a08134a50403
mselle14 <mselle14@aum.edu>
2026-04-09 15:02:38 -0500

    Update e wallet top up.cpp

--- e wallet top up.cpp
+++ e wallet top up.cpp
@@ -1,20 +1,63 @@
-// e wallet top up.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Marshon Sellers
 
 #include <iostream>
+using namespace std;
+
+class EWallet
+{
+private:
+    int balance;
+
+public:
+    EWallet()
+    {
+        balance = 0;
+    }
+
+    void set_balance(int new_balance)
+    {
+        if (new_balance >= 0)
+        {
+            balance = new_balance;
+        }
+        else
+        {
+            cout << "Invalid balance!" << endl;
+        }
+    }
+
+    int get_balance()
+    {
+        return balance;
+    }
+
+    void spend(int amount)
+    {
+        if (amount > 0 && amount <= balance)
+        {
+            balance -= amount;
+        }
+        else
+        {
+            cout << "Payment declined!" << endl;
+        }
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    EWallet wallet;
+
+    int initial_balance;
+    int spend_amount;
+
+    cin >> initial_balance;
+    cin >> spend_amount;
+
+    wallet.set_balance(initial_balance);
+    wallet.spend(spend_amount);
+
+    cout << "Final balance: " << wallet.get_balance() << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
